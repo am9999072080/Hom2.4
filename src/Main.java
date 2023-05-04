b0a17327e7073afe19ee4e169832db25bea0c5e7
am999 <am9999072080@gmail.com>
2023-05-04 23:12:36 +0300

    commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import exception.WrongLoginException;
+import exception.WrongPasswordException;
+
+public class Main {
+    private static final String validatePattern = "^[a-zA-Z0-9_]*$";
+    public static void main(String[] args) {
+   
+    }
+}
